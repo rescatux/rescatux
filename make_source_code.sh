65b54cd41dc668224c020ece791b6decf58e412e
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2010-12-06 15:35:11 +0100

    Initial update-grub option as a clone of grub-install option

--- make_source_code.sh
+++ make_source_code.sh
@@ -6,7 +6,7 @@ CURRENT_FOLDER=$PWD
 BASE_FILENAME="rescatux_source_code_`date +%Y_%m_%d`"
 TEMP_FOLDER_PRE="/tmp/$$"
 TEMP_FOLDER="${TEMP_FOLDER_PRE}/${BASE_FILENAME}"
-SOURCE_CODE_FILE="README Makefile  rescapp.sh  rescapp.sh.desktop make_source_code.sh make_rescatux_cd.sh make_rescatux_hybrid_amd64.sh make_rescatux_hybrid_i486.sh make_rescatux_hybrid.sh make_rescatux_usb.sh make_common chat web grub-install grub.lis grub rescatux.lis log show_log support support.lis logo"
+SOURCE_CODE_FILE="README Makefile  rescapp.sh  rescapp.sh.desktop make_source_code.sh make_rescatux_cd.sh make_rescatux_hybrid_amd64.sh make_rescatux_hybrid_i486.sh make_rescatux_hybrid.sh make_rescatux_usb.sh make_common chat web grub-install update-grub grub.lis grub rescatux.lis log show_log support support.lis logo"
 mkdir --parents $TEMP_FOLDER
 cp -r ${SOURCE_CODE_FILE} $TEMP_FOLDER
 cd ${TEMP_FOLDER_PRE}
