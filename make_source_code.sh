fa8fd05b7f4c97790dbaa868140a4e43b553cb9b
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2011-01-02 20:16:04 +0100

    Source code also is based on version file

--- make_source_code.sh
+++ make_source_code.sh
@@ -3,7 +3,7 @@ set -x
 set -v
 
 CURRENT_FOLDER=$PWD
-BASE_FILENAME="rescatux_source_code_`date +%Y_%m_%d`"
+BASE_FILENAME="rescatux_source_code_`head -n 1 VERSION`"
 TEMP_FOLDER_PRE="/tmp/$$"
 TEMP_FOLDER="${TEMP_FOLDER_PRE}/${BASE_FILENAME}"
 SOURCE_CODE_FILE="VERSION README Makefile  rescapp.sh  rescapp.sh.desktop make_source_code.sh make_rescatux_cd.sh make_rescatux_hybrid_amd64.sh make_rescatux_hybrid_i486.sh make_rescatux_hybrid.sh make_rescatux_usb.sh make_common chat web grub-install update-grub grub.lis grub rescatux.lis log show_log support support.lis logo"
