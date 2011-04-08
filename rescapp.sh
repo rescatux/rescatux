0fb831e3ee90805d2a8d4475fe8499f2c54836c6
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2011-04-08 15:06:13 +0200

    Idented rescapp.sh in a nice manner.

--- rescapp.sh
+++ rescapp.sh
@@ -2,97 +2,106 @@
 
 function show_menu() {
 
-OPTIONS_FILE=${1}.${LIST_FILE_SUFFIX}
-MENU_TITLE=$1
+  OPTIONS_FILE=${1}.${LIST_FILE_SUFFIX}
+  MENU_TITLE=$1
 
-choice="$(find `cat ${OPTIONS_FILE}` -maxdepth 1 -type d -exec cat {}/$DIRECTORY_FILE_STR {}/$NAME_FILE_STR {}/$DESCRIPTION_FILE_STR \; | zenity --width=${RESCAPP_WIDTH} --height=${RESCAPP_HEIGHT} --list --column "${CODE_STR}" --column "${NAME_STR}" --column "${DESCRIPTION_STR}" --title="${MENU_TITLE}" \
-)";
+  choice="$(find `cat ${OPTIONS_FILE}` -maxdepth 1 -type d \
+	    -exec cat {}/$DIRECTORY_FILE_STR {}/$NAME_FILE_STR {}/$DESCRIPTION_FILE_STR \; \
+	    | zenity \
+		--width=${RESCAPP_WIDTH} \
+		--height=${RESCAPP_HEIGHT} \
+		--list --column "${CODE_STR}" \
+		--column "${NAME_STR}" \
+		--column "${DESCRIPTION_STR}" \
+		--title="${MENU_TITLE}" \
+  )";
 
-result=$?
+  result=$?
 
-case $result in
-0)
-  show_item $choice
-;;
-1)
-
-exit;
-;;
-esac
-
-}
+  case $result in
+  0)
+    show_item $choice
+  ;;
+  1)
+    exit;
+  ;;
+  esac
+} # show_menu function
 
 function show_item() {
 
-DIRECTORY=$1
+  DIRECTORY=$1
 
-unset zenity_option_line
-declare -a zenity_option_line
-let n_zenity_option_line=0
-
-
-if [ -e ${DIRECTORY}.${LIST_FILE_SUFFIX} ] ; then
-  show_menu ${DIRECTORY}
-else
-
-  # Add run option if there is the run file
-  if [ -e ${DIRECTORY}/${RUN_FILE_STR} ] ; then
-    zenity_option_line[$n_zenity_option_line]="${RUN_CODE}"
-    let n_zenity_option_line=n_zenity_option_line+1
-    zenity_option_line[$n_zenity_option_line]="${RUN_STR}"
-    let n_zenity_option_line=n_zenity_option_line+1
-  fi
-
-  # Add local documentation option if there is the local_doc.html file
-  if [ -e ${DIRECTORY}/${LOCAL_FILE_STR} ] ; then
-    zenity_option_line[$n_zenity_option_line]="${LOCAL_DOC_CODE}"
-    let n_zenity_option_line=n_zenity_option_line+1
-    zenity_option_line[$n_zenity_option_line]="${LOCAL_DOC_STR}"
-    let n_zenity_option_line=n_zenity_option_line+1
-  fi
-
-  # Add online documentation option if there is the online_doc.html file
-  if [ -e ${DIRECTORY}/${ONLINE_FILE_STR} ] ; then
-    zenity_option_line[$n_zenity_option_line]="${ONLINE_DOC_CODE}"
-    let n_zenity_option_line=n_zenity_option_line+1
-    zenity_option_line[$n_zenity_option_line]="${ONLINE_DOC_STR}"
-    let n_zenity_option_line=n_zenity_option_line+1
-  fi
+  unset zenity_option_line
+  declare -a zenity_option_line
+  let n_zenity_option_line=0
 
 
-     choice="$(zenity --width=${RESCAPP_WIDTH} --height=${RESCAPP_HEIGHT} --list --column "${CODE_STR}" --column "${DESCRIPTION_STR}" --title="${DIRECTORY}" "${zenity_option_line[@]}")"
-     result=$?
+  if [ -e ${DIRECTORY}.${LIST_FILE_SUFFIX} ] ; then
+    show_menu ${DIRECTORY}
+  else
 
-SUDO="sudo"
-[ -e ${DIRECTORY}/sudo ] || SUDO=""
+    # Add run option if there is the run file
+    if [ -e ${DIRECTORY}/${RUN_FILE_STR} ] ; then
+      zenity_option_line[$n_zenity_option_line]="${RUN_CODE}"
+      let n_zenity_option_line=n_zenity_option_line+1
+      zenity_option_line[$n_zenity_option_line]="${RUN_STR}"
+      let n_zenity_option_line=n_zenity_option_line+1
+    fi
 
-case $result in
-0)
+    # Add local documentation option if there is the local_doc.html file
+    if [ -e ${DIRECTORY}/${LOCAL_FILE_STR} ] ; then
+      zenity_option_line[$n_zenity_option_line]="${LOCAL_DOC_CODE}"
+      let n_zenity_option_line=n_zenity_option_line+1
+      zenity_option_line[$n_zenity_option_line]="${LOCAL_DOC_STR}"
+      let n_zenity_option_line=n_zenity_option_line+1
+    fi
+
+    # Add online documentation option if there is the online_doc.html file
+    if [ -e ${DIRECTORY}/${ONLINE_FILE_STR} ] ; then
+      zenity_option_line[$n_zenity_option_line]="${ONLINE_DOC_CODE}"
+      let n_zenity_option_line=n_zenity_option_line+1
+      zenity_option_line[$n_zenity_option_line]="${ONLINE_DOC_STR}"
+      let n_zenity_option_line=n_zenity_option_line+1
+    fi
 
 
-	case $choice in
-	${RUN_CODE})
+    choice="$(zenity \
+	  --width=${RESCAPP_WIDTH} \
+	  --height=${RESCAPP_HEIGHT} \
+	  --list \
+	  --column "${CODE_STR}" \
+	  --column "${DESCRIPTION_STR}" \
+	  --title="${DIRECTORY}" \
+	  "${zenity_option_line[@]}" \
+    )";
+
+    result=$?
+
+    SUDO="sudo"
+    [ -e ${DIRECTORY}/sudo ] || SUDO=""
+
+    case $result in
+    0)
+      case $choice in
+      ${RUN_CODE})
 	exec ${SUDO} ${DIRECTORY}/${RUN_FILE_STR} > ${LOG_DIRECTORY}/${DIRECTORY}_log.txt 2>&1 &
-	;;
-	${LOCAL_DOC_CODE})
+      ;;
+      ${LOCAL_DOC_CODE})
 	${FIREFOX_COMMAND} --new-window ${DIRECTORY}/${LOCAL_DOC_STR} &
-	;;
-	${ONLINE_DOC_CODE})
+      ;;
+      ${ONLINE_DOC_CODE})
 	${FIREFOX_COMMAND} --new-window ${DIRECTORY}/${ONLINE_DOC_STR} &
-	;;
-	esac
+      ;;
+      esac
+    ;;
+    1)
+      return;
+    ;;
+    esac
 
-;;
-1)
-
-return;
-;;
-esac
-
-fi
-
-
-}
+  fi # [ -e ${DIRECTORY}.${LIST_FILE_SUFFIX} ] (else)
+} # show_item function
 
 
 # Main program
@@ -136,20 +145,28 @@ RESCAPP_TITLE_STR="RESCATUX's RESCAPP"
 cd ${DEFAULT_PATH}
 
 while true; do
-choice="$(find `cat rescatux.lis` -maxdepth 1 -type d -exec cat {}/directory {}/name {}/description \; | zenity --width=${RESCAPP_WIDTH} --height=${RESCAPP_HEIGHT} --list --column "${CODE_STR}" --column "${NAME_STR}" --column "${DESCRIPTION_STR}" --title="${RESCAPP_TITLE_STR}" \
-)";
+  choice="$(find `cat rescatux.lis` -maxdepth 1 -type d \
+  -exec cat {}/directory {}/name {}/description \; \
+  | zenity \
+    --width=${RESCAPP_WIDTH} \
+    --height=${RESCAPP_HEIGHT} \
+    --list \
+    --column "${CODE_STR}" \
+    --column "${NAME_STR}" \
+    --column "${DESCRIPTION_STR}" \
+    --title="${RESCAPP_TITLE_STR}" \
+  )";
 
-result=$?
+  result=$?
 
-case $result in
-0)
-  show_item $choice
-;;
-1)
+  case $result in
+  0)
+    show_item $choice
+  ;;
+  1)
+    exit;
+  ;;
+  esac
 
-exit;
-;;
-esac
-
-done
+done # while true
 
