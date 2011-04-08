ef2ed1b7acc89d06325d14d010e361fd08baa4bc
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2011-04-08 18:48:23 +0200

    Code that implements functions for getting system partitions and linux os partitions was niced.

--- rescatux_lib.sh
+++ rescatux_lib.sh
@@ -5,29 +5,30 @@
 function rtux_Get_System_Partitions () {
   awk '{print $4}' ${PROC_PARTITIONS_FILE} \
   | sed -e '/name/d' -e '/^$/d' -e '/[1-9]/!d'
-}
+} # function rtux_Get_System_Partitions ()
 
 # Return partitions which have Linux os detector on them
 function rtux_Get_Linux_Os_Partitions() {
-local TARGET_PARTITIONS=$(rtux_Get_System_Partitions())
-local SBIN_GRUB_PARTITIONS=""
+  local TARGET_PARTITIONS=$(rtux_Get_System_Partitions())
+  local SBIN_GRUB_PARTITIONS=""
 
-for n_partition in ${TARGET_PARTITIONS}; do
-  local TMP_MNT_PARTITION=${RESCATUX_ROOT_MNT}/${n_partition}
-  local TMP_DEV_PARTITION=/dev/${n_partition}
-  mkdir --parents ${TMP_MNT_PARTITION}
+  for n_partition in ${TARGET_PARTITIONS}; do
+    local TMP_MNT_PARTITION=${RESCATUX_ROOT_MNT}/${n_partition}
+    local TMP_DEV_PARTITION=/dev/${n_partition}
+    mkdir --parents ${TMP_MNT_PARTITION}
 
-  if $(mount -t auto ${TMP_DEV_PARTITION} ${TMP_MNT_PARTITION} 2> /dev/null) ; then
-    if [[ -e ${TMP_MNT_PARTITION}${LINUX_OS_DETECTOR} ]] ; then
-      SBIN_GRUB_PARTITIONS="${SBIN_GRUB_PARTITIONS} ${n_partition}"
+    if $(mount -t auto ${TMP_DEV_PARTITION} ${TMP_MNT_PARTITION} 2> /dev/null) ;
+    then
+      if [[ -e ${TMP_MNT_PARTITION}${LINUX_OS_DETECTOR} ]] ; then
+        SBIN_GRUB_PARTITIONS="${SBIN_GRUB_PARTITIONS} ${n_partition}"
+      fi
+      umount ${TMP_MNT_PARTITION};
     fi
-    umount ${TMP_MNT_PARTITION};
-  fi
-done
+  done
 
-echo "${SBIN_GRUB_PARTITIONS}"
+  echo "${SBIN_GRUB_PARTITIONS}"
 
-}
+} # function rtux_Get_System_Partitions ()
 
 
 # Returns Desktop width
