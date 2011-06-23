0b1194e3a6db5307bd437b7bba9dfbc0ab2038ce
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2011-06-23 18:24:29 +0200

    Lxde minor improvments.

--- setbackground.sh
+++ setbackground.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 LIVE_HOME="/home/user"
 MAGIC_BACKGROUND_FILENAME="background.png"
-gconftool-2 -t str --set /desktop/gnome/background/picture_filename "${LIVE_HOME}/Desktop/logos/${MAGIC_BACKGROUND_FILENAME}"
+pcmanfm --set-wallpaper "${LIVE_HOME}/Desktop/logos/${MAGIC_BACKGROUND_FILENAME}"
 
