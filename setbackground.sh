835276291b697f8cb3340a2dc014b6e3b484e51e
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2011-06-24 23:24:39 +0200

    Modified set background script so that it also setups file association in lxde. We want txt files to be opened with leafpad.

--- setbackground.sh
+++ setbackground.sh
@@ -2,4 +2,7 @@
 LIVE_HOME="/home/user"
 MAGIC_BACKGROUND_FILENAME="background.png"
 pcmanfm --set-wallpaper "${LIVE_HOME}/Desktop/logos/${MAGIC_BACKGROUND_FILENAME}"
-
+cat << EOF > ${LIVE_HOME}/.local/share/applications/mimeapps.list
+[Added Associations]
+text/plain=leafpad.desktop;
+EOF
