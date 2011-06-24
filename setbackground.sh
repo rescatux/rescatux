738108b703e9d558c8b57fc9916a0f70b8d4bd61
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2011-06-25 00:07:07 +0200

    Set background script was updated to the new Rescapp location

--- setbackground.sh
+++ setbackground.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 LIVE_HOME="/home/user"
 MAGIC_BACKGROUND_FILENAME="background.png"
-pcmanfm --set-wallpaper "${LIVE_HOME}/Desktop/logos/${MAGIC_BACKGROUND_FILENAME}"
+pcmanfm --set-wallpaper "${LIVE_HOME}/Desktop/rescapp/logos/${MAGIC_BACKGROUND_FILENAME}"
 cat << EOF > ${LIVE_HOME}/.local/share/applications/mimeapps.list
 [Added Associations]
 text/plain=leafpad.desktop;
