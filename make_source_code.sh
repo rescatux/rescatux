a7b5a49abab06e7ef54cc8b24b7eb50391ef6cdd
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2011-06-24 16:35:01 +0200

    Make Boot Info script appear on cd and on source code.

--- make_source_code.sh
+++ make_source_code.sh
@@ -6,7 +6,7 @@ CURRENT_FOLDER=$PWD
 BASE_FILENAME="rescatux_source_code_`head -n 1 VERSION`"
 TEMP_FOLDER_PRE="/tmp/$$"
 TEMP_FOLDER="${TEMP_FOLDER_PRE}/${BASE_FILENAME}"
-SOURCE_CODE_FILE="VERSION README Makefile  rescapp.sh  rescapp.sh.desktop make_source_code.sh make_rescatux_cd.sh make_rescatux_hybrid_amd64.sh make_rescatux_hybrid_i486.sh make_rescatux_hybrid.sh make_rescatux_usb.sh make_common chat web grub-install update-grub grub.lis grub rescatux.lis log show_log support support.lis logo setbackground.sh setbackground.sh.desktop about.lis about about-rescapp rescatux_lib.sh share_log win.lis win winmbr winpass fs fs.lis fsck"
+SOURCE_CODE_FILE="VERSION README Makefile  rescapp.sh  rescapp.sh.desktop make_source_code.sh make_rescatux_cd.sh make_rescatux_hybrid_amd64.sh make_rescatux_hybrid_i486.sh make_rescatux_hybrid.sh make_rescatux_usb.sh make_common chat web grub-install update-grub grub.lis grub rescatux.lis log show_log support support.lis logo setbackground.sh setbackground.sh.desktop about.lis about about-rescapp rescatux_lib.sh share_log win.lis win winmbr winpass fs fs.lis fsck bootinfoscript"
 mkdir --parents $TEMP_FOLDER
 cp -r ${SOURCE_CODE_FILE} $TEMP_FOLDER
 cd ${TEMP_FOLDER_PRE}
