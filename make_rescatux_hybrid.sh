2830b1e10ae0a5dbb592c862925581abdcf23a9a
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2011-06-24 17:24:55 +0200

    Added gawk command so that boot info script runs ok.

--- make_rescatux_hybrid.sh
+++ make_rescatux_hybrid.sh
@@ -6,7 +6,7 @@ MEDIA_STR="cdrom_usb_hybrid"
 FILE_EXTENSION="iso"
 LXDE_PACKAGES="lxde gdm3 network-manager-gnome"
 #GNOME_MINIMAL_PACKAGES="gdm3 gedit gnome-core nautilus"
-PACKAGES="${LXDE_PACKAGES} zenity iceweasel xchat syslinux pastebinit mbr ntfs-3g chntpw samdump2 bkhive" # I add syslinux so that isohybrid command is recognised.
+PACKAGES="${LXDE_PACKAGES} zenity iceweasel xchat syslinux pastebinit mbr ntfs-3g chntpw samdump2 bkhive gawk" # I add syslinux so that isohybrid command is recognised.
 BOOT_OPTION="-b iso-hybrid"
 LINUX_FLAVOURS="486 amd64"
 source make_common
