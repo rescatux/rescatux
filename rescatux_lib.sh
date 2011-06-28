rescatux/rescatux

commit 6fa4d88ace3e37d6122668189f9d55289ad3545a

rescatux_lib: Rewrite and simplify rtux_Get_System_HardDisks function with awk.
rescatux_lib: Rewrite and simplify rtux_Get_System_HardDisks function with awk.

awk will keep only the device names which last character is a character
from the latin alphabeth.

The hard disk names will be separated by a space.

Signed-off-by: Gert Hulselmans <gerth@users.berlios.de>

rescatux_lib.sh
@@ -89,11 +89,8 @@ function rtux_Get_Windows_Os_Partitions() {
 # Return hard disks detected on the system
 function rtux_Get_System_HardDisks () {
   local TARGET_PARTITIONS=$(rtux_Get_System_Partitions)
-  echo $(echo ${TARGET_PARTITIONS} \
-	  | sed 's/[0-9][0-9]*//g' \
-	  | tr ' ' '\n' \
-	  | uniq \
-	  | tr '\n' ' ');
+
+  echo ${TARGET_PARTITIONS}  | awk '$1 ~ "[[:alpha:]]$" { printf $1 " " }'
 } # function rtux_Get_System_HardDisks ()
 
 # Informs the user about an operation that has been successful

commit 886395367289575b301115d61ca3b22bfa8b1d2f

rescatux_lib: Fix rtux_Get_System_HardDisks function and detect all hard disks.
rescatux_lib: Fix rtux_Get_System_HardDisks function and detect all hard disks.

Commit 6fa4d88ace3e37d6122668189f9d55289ad3545a broke the hard disk detection
completely.

Now the hard disks are directly retrieved from /proc/partitions and all hard
disks, even if they don't contain a partition, are displayed.

Signed-off-by: Gert Hulselmans <gerth@users.berlios.de>

rescatux_lib.sh
@@ -88,9 +88,7 @@ function rtux_Get_Windows_Os_Partitions() {
 
 # Return hard disks detected on the system
 function rtux_Get_System_HardDisks () {
-  local TARGET_PARTITIONS=$(rtux_Get_System_Partitions)
-
-  echo ${TARGET_PARTITIONS}  | awk '$1 ~ "[[:alpha:]]$" { printf $1 " " }'
+  awk '{ if ( ( NR>2 ) && ( $4 ~ "[[:alpha:]]$" ) ) {print $4} }' ${PROC_PARTITIONS_FILE}
 } # function rtux_Get_System_HardDisks ()
 
 # Informs the user about an operation that has been successful