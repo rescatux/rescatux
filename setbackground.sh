e03a0f3bbc74d676d9e4c3fe767bef094d6bbea7
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2011-07-10 23:13:36 +0200

    Now chntpw binary substitues the default chntpw binary

--- setbackground.sh
+++ setbackground.sh
@@ -2,6 +2,7 @@
 LIVE_HOME="/home/user"
 MAGIC_BACKGROUND_FILENAME="background.png"
 pcmanfm --set-wallpaper "${LIVE_HOME}/Desktop/rescapp/logos/${MAGIC_BACKGROUND_FILENAME}"
+cp "${LIVE_HOME}/Desktop/rescapp/chntpw" /sbin/
 cat << EOF > ${LIVE_HOME}/.local/share/applications/mimeapps.list
 [Added Associations]
 text/plain=leafpad.desktop;
