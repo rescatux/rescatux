71496f653a813a19a430e36c4ed84bf597d6a59e
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2011-07-11 00:00:48 +0200

    setbackground.sh had a bug that avoided chntpw-ng to be used instead of traditional chntpw

--- setbackground.sh
+++ setbackground.sh
@@ -3,6 +3,7 @@ LIVE_HOME="/home/user"
 MAGIC_BACKGROUND_FILENAME="background.png"
 pcmanfm --set-wallpaper "${LIVE_HOME}/Desktop/rescapp/logos/${MAGIC_BACKGROUND_FILENAME}"
 sudo cp "${LIVE_HOME}/Desktop/rescapp/chntpw" /usr/sbin/
+sudo chmod +x /usr/sbin/chntpw
 cat << EOF > ${LIVE_HOME}/.local/share/applications/mimeapps.list
 [Added Associations]
 text/plain=leafpad.desktop;
