e63d2e88b4328318c6f7cc675d841d87c4cd88cd
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2011-11-07 00:46:25 +0100

    rescapp_launcher.sh was not found. Fixed that error.

--- rescapp.py
+++ rescapp.py
@@ -110,7 +110,7 @@ class MainWindow(QtGui.QWidget):
 	
     def runRescue(self):
 	print "DEBUG: Running option (code) [BEGIN]: " + self.selected_option_code
-	run_status = subprocess.call(["./rescapp_launcher.sh",self.selected_option_code])
+	run_status = subprocess.call([current_pwd + "/" + "rescapp_launcher.sh",self.selected_option_code])
 	print "DEBUG: Running option (code) [FINISH]: " + self.selected_option_code + " with code: " + str(run_status);
 
     def setLayout(self, layout):
