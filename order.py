c156db61fea85fa7ac2327dfded3a32450c740d0
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2011-12-06 12:41:50 +0100

    Order python dialog added but not integrated into Rescapp

---
+++ order.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# order.py for Rescapp
+
+import sys, subprocess, os, time, linecache, sip, getopt
+from PyQt4 import QtGui,QtCore,QtWebKit
+from PyQt4.QtGui import QIcon
+
+class MainWindow(QtGui.QWidget):
+
+    def get_qicon(self,name):
+      if not name:
+	  return QIcon()
+      
+      if os.path.isabs(name):
+	  return QIcon(name)
+      
+      icon = QIcon.fromTheme(name)
+      if not icon.isNull():
+	  return icon
+	  
+      for i in os.listdir("/usr/share/pixmaps")[::-1]:
+	  if i.startswith(name):
+	      path = os.path.join("/usr/share/pixmaps", i)
+	      return QIcon(path)
+    
+      return QIcon()
+
+    def setLayout(self, layout):
+	self.clearLayout()
+	QtGui.QWidget.setLayout(self, layout)
+
+    def clearLayout(self):
+	if self.layout() is not None:
+	    old_layout = self.layout()
+	    for i in reversed(range(old_layout.count())):
+		old_layout.itemAt(i).widget().setParent(None)
+	    sip.delete(old_layout)
+
+    def refresh_harddisks(self):
+	#print "DEBUG: rh"
+        while self.harddisks_tablewidget.rowCount(): 
+            self.harddisks_tablewidget.removeRow(0)
+        n = 0
+        m = 0
+        for harddisk in harddisks:
+	    self.harddisks_tablewidget.insertRow(self.harddisks_tablewidget.rowCount())
+	    m = 0
+	    for cell in harddisk:
+	      item = QtGui.QTableWidgetItem(cell)
+	      self.harddisks_tablewidget.setItem(n,m,item)
+	      m +=1  
+	    n +=1
+	    
+	self.harddisks_tablewidget.setColumnCount(m)
+
+            
+    def move_up_button_clicked(self):
+	global harddisks
+        current = self.harddisks_tablewidget.currentRow()
+        
+        if current == -1:
+            return 
+        
+        item = harddisks.pop(current)
+        harddisks.insert(current-1, item)
+        
+        self.refresh_harddisks()
+        
+        self.harddisks_tablewidget.setCurrentCell(self.harddisks_tablewidget.rowCount()-1,self.harddisks_tablewidget.currentColumn())
+    def move_down_button_clicked(self):
+	global harddisks
+        current = self.harddisks_tablewidget.currentRow()
+        
+        if current == -1:
+            return 
+        
+        item = harddisks.pop(current)
+        harddisks.insert(current+1, item)
+        
+        self.refresh_harddisks()
+        
+        self.harddisks_tablewidget.setCurrentCell(self.harddisks_tablewidget.rowCount()+1,self.harddisks_tablewidget.currentColumn())
+
+    def selected_harddisk_changed(self):                       
+
+        current = self.harddisks_tablewidget.currentRow()
+
+        self.move_up_button.setEnabled(True)
+        self.move_down_button.setEnabled(True)
+        
+        if current == 0:
+            self.move_up_button.setEnabled(False)
+        if current == self.harddisks_tablewidget.rowCount()-1:
+            self.move_down_button.setEnabled(False)
+	  
+	  
+
+
+    def drawMainWindows(self):
+	global harddisks
+	global description
+      
+	
+	self.okbtn = QtGui.QPushButton('Ok', self)
+        self.okbtn.clicked.connect(self.close)
+        self.okbtn.resize(self.okbtn.sizeHint())
+	
+	# Initialize hard disk stuff - BEGIN
+	self.harddisks_tablewidget = QtGui.QTableWidget()
+	self.harddisks_tablewidget.itemSelectionChanged.connect(self.selected_harddisk_changed)
+
+	self.harddisks_tablewidget.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows);
+	self.harddisks_tablewidget.setSelectionMode(QtGui.QAbstractItemView.SingleSelection);
+	# Initialize hard disk stuff - END
+	# Move up button - BEGIN
+	self.move_up_button = QtGui.QToolButton();
+	self.move_up_button.setIcon(self.get_qicon("go-up"))
+	self.move_up_button.clicked.connect(self.move_up_button_clicked)
+	# Move up button - END
+	
+	# Move down button - BEGIN
+	self.move_down_button = QtGui.QToolButton();
+	self.move_down_button.setIcon(self.get_qicon("go-down"))
+	self.move_down_button.clicked.connect(self.move_down_button_clicked)
+	# Move down button - END
+	self.refresh_harddisks()
+	# Add button - BEGIN
+
+	self.descriptionwidget = QtGui.QLabel(description)
+	self.descriptionwidget.setWordWrap(1==1)
+
+        grid = QtGui.QGridLayout()
+        grid.setSpacing(10)
+
+	grid.addWidget(self.descriptionwidget, 0, 0, 1, 3)
+	grid.addWidget(self.harddisks_tablewidget, 1, 2, 10, 10)
+	grid.addWidget(self.move_up_button, 1, 0, 1, 1)
+	grid.addWidget(self.move_down_button, 1, 1, 1, 1)
+	grid.addWidget(self.okbtn,2,0,1,1)
+        
+        #grid.setMargin(0)
+        #grid.setContentsMargins(0,0,0,0)
+	self.setLayout(grid)
+
+	self.setMaximumHeight(570)
+
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+    def closeEvent(self,event):
+      for harddisk in harddisks:
+	print(harddisk[0])
+      event.accept()
+
+
+if __name__ == "__main__":
+ 
+    app=QtGui.QApplication(sys.argv)
+    
+    harddisks = []
+    label_list = []
+    mw=MainWindow()
+    
+    
+    
+    myargs = sys.argv[1:]
+    # Parse number of columns
+    column_number=int(sys.argv[1])
+    #print "Column number: " + str(column_number)
+    #print myargs
+    
+    myargs.pop(0)
+    
+    # Parse window title
+    window_title = myargs[0]
+    myargs.pop(0)
+    # Parse description
+    description = myargs[0]
+    myargs.pop(0)
+    
+    mw.drawMainWindows()
+    
+    # Parse label descriptions
+    ncolumn=0
+    while (ncolumn < column_number):
+      label_descr = myargs[0]
+      label_list.append(label_descr)
+      myargs.pop(0)
+      ncolumn+=1
+    #print "labels: " + str(label_list)
+    # Parse contents
+    row=0
+    while myargs:
+      new_harddisk = []
+      ncolumn=0
+      while (ncolumn < column_number):
+	new_harddisk_descr = myargs[0]
+	new_harddisk.append(new_harddisk_descr)
+	myargs.pop(0)
+	ncolumn+=1
+      harddisks.append(new_harddisk)
+      mw.refresh_harddisks()
+      row+=1
+    #print harddisks
+    mw.setWindowTitle(window_title)
+    
+    mw.show()
+    sys.exit(app.exec_())
