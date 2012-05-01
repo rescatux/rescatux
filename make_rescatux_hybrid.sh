642cf635d80effd5ebd593524323865c7cb273b5
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2012-05-01 14:10:59 +0200

    Added backported passwd package dependencies

--- make_rescatux_hybrid.sh
+++ make_rescatux_hybrid.sh
@@ -8,8 +8,9 @@ LXDE_PACKAGES="lxde gdm3 network-manager-gnome"
 PYTHON_PACKAGES="python-sip python-qt4"
 RAID_PACKAGES="dmraid"
 LVM_PACKAGES="lvm2"
+PASSWD_PACKAGES="libsemanage1 libsemanage-common libustr-1.0-1"
 #GNOME_MINIMAL_PACKAGES="gdm3 gedit gnome-core nautilus"
-PACKAGES="${LXDE_PACKAGES} ${PYTHON_PACKAGES} ${RAID_PACKAGES} ${LVM_PACKAGES} zenity iceweasel xchat syslinux pastebinit mbr ntfs-3g chntpw samdump2 bkhive gawk" # I add syslinux so that isohybrid command is recognised.
+PACKAGES="${LXDE_PACKAGES} ${PYTHON_PACKAGES} ${RAID_PACKAGES} ${LVM_PACKAGES} ${PASSWD_PACKAGES} zenity iceweasel xchat syslinux pastebinit mbr ntfs-3g chntpw samdump2 bkhive gawk" # I add syslinux so that isohybrid command is recognised.
 BOOT_OPTION="-b iso-hybrid"
 LINUX_FLAVOURS="486 amd64"
 source make_common
