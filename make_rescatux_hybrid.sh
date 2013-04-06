ed93f0866c6b0a1fa71e6f0f8ef231bd233c5d10
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2013-04-06 15:34:48 +0200

    Adapted make_rescatux_hybrid.sh to Wheezy
    * Hybrid cdrom filename part was changed from -hybrid to .hybrid.
    * Lxde packages has been replaced by task-lxde-desktop metapackage so we only keep network-manager-gnome package so that wifi network can be setup easily.
    * Simplified Gksu packages
    * Simplified Gparted packages
    * Passwd packages needed in Squeeze because we used a backported Wheezy passwd no longer needed. Removed.
    * Bootinfo packages removed because xz-utils packages which replaces xz-lzma package is installed by default.
    * Removed commented, no longer used, gnome minimal packages

--- make_rescatux_hybrid.sh
+++ make_rescatux_hybrid.sh
@@ -17,20 +17,17 @@
 
 # This script assumes apt-get install live-helper has been done
 # This script assumes that the user has sudo permissions on lh build
-IS_HYBRID="-hybrid"
+IS_HYBRID=".hybrid"
 MEDIA_STR="cdrom_usb_hybrid"
 FILE_EXTENSION="iso"
-LXDE_PACKAGES="lxde gdm3 network-manager-gnome"
+LXDE_PACKAGES="network-manager-gnome"
 PYTHON_PACKAGES="python-sip python-qt4"
 RAID_PACKAGES="dmraid"
 LVM_PACKAGES="lvm2"
 SYNAPTIC_PACKAGES="synaptic"
-GKSU_PACKAGES="libgtop2-common libgtop2-7 libgksu2-0 gksu"
-GPARTED_PACKAGES="libcairomm-1.0-1 libglibmm-2.4-1c2a libpangomm-1.4-1 libgtkmm-2.4-1c2a gparted"
-PASSWD_PACKAGES="libsemanage1 libsemanage-common libustr-1.0-1"
-BOOTINFO_PACKAGES="xz-lzma"
-#GNOME_MINIMAL_PACKAGES="gdm3 gedit gnome-core nautilus"
-PACKAGES="${LXDE_PACKAGES} ${PYTHON_PACKAGES} ${RAID_PACKAGES} ${LVM_PACKAGES} ${PASSWD_PACKAGES} ${SYNAPTIC_PACKAGES} ${GKSU_PACKAGES} ${GPARTED_PACKAGES} ${BOOTINFO_PACKAGES} zenity iceweasel xchat syslinux pastebinit mbr ntfs-3g chntpw samdump2 bkhive gawk" # I add syslinux so that isohybrid command is recognised.
+GKSU_PACKAGES="gksu"
+GPARTED_PACKAGES="gparted"
+PACKAGES="${LXDE_PACKAGES} ${PYTHON_PACKAGES} ${RAID_PACKAGES} ${LVM_PACKAGES} ${SYNAPTIC_PACKAGES} ${GKSU_PACKAGES} ${GPARTED_PACKAGES} zenity iceweasel xchat syslinux pastebinit mbr ntfs-3g chntpw samdump2 bkhive gawk" # I add syslinux so that isohybrid command is recognised.
 BOOT_OPTION="-b iso-hybrid"
 LINUX_FLAVOURS="amd64 486"
 source make_common
