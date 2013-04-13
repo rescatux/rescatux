733016f79f80eef769f73f8cafd70c05240e91f7
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2013-04-13 18:31:47 +0200

    * Raw razor-qt added to Rescatux * Removed task-lxde-desktop package * Added SID Mirror * Distribution is now testing instead of wheezy so that sid packages can be pinned ok * sudo lb config to avoid permissions problems

--- make_rescatux_hybrid.sh
+++ make_rescatux_hybrid.sh
@@ -20,14 +20,14 @@
 IS_HYBRID=".hybrid"
 MEDIA_STR="cdrom_usb_hybrid"
 FILE_EXTENSION="iso"
-LXDE_PACKAGES="network-manager-gnome"
+RAZORQT_PACKAGES="network-manager-gnome"
 PYTHON_PACKAGES="python-sip python-qt4"
 RAID_PACKAGES="dmraid"
 LVM_PACKAGES="lvm2"
 SYNAPTIC_PACKAGES="synaptic"
 GKSU_PACKAGES="gksu"
 GPARTED_PACKAGES="gparted gpart"
-PACKAGES="${LXDE_PACKAGES} ${PYTHON_PACKAGES} ${RAID_PACKAGES} ${LVM_PACKAGES} ${SYNAPTIC_PACKAGES} ${GKSU_PACKAGES} ${GPARTED_PACKAGES} zenity iceweasel xchat syslinux pastebinit mbr ntfs-3g chntpw samdump2 bkhive gawk" # I add syslinux so that isohybrid command is recognised.
+PACKAGES="${RAZORQT_PACKAGES} ${PYTHON_PACKAGES} ${RAID_PACKAGES} ${LVM_PACKAGES} ${SYNAPTIC_PACKAGES} ${GKSU_PACKAGES} ${GPARTED_PACKAGES} zenity iceweasel xchat syslinux pastebinit mbr ntfs-3g chntpw samdump2 bkhive gawk" # I add syslinux so that isohybrid command is recognised.
 BOOT_OPTION="-b iso-hybrid"
 LINUX_FLAVOURS="amd64 486"
 source make_common
