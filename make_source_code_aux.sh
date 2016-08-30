f43b58bab93c56630e7998c1825be830921745d6
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2016-08-30 21:04:40 +0200

    Source code build improvements:
    * Updated README to match new source code build instructions
    * Make sure rescatux-release/source-code directory is created
    * Make sure md5sum files from source code are created ok.
    * Added new function: get_Git_Tar_Gz to make_source_code_aux.sh script to make easier to get Git contents into tar.gz
    * Used get_Git_Tar_Gz to get chntpw git

--- make_source_code_aux.sh
+++ make_source_code_aux.sh
@@ -18,6 +18,43 @@
 set -x
 set -v
 
+
+# 1 parametre = Git url
+# 2 parametre = Git commit
+# 3 parametre = Final Tar.gz
+function get_Git_Tar_Gz () {
+  local GIT_URL="$1"
+  local GIT_COMMIT="$2"
+  local FINAL_TAR_GZ="$3"
+  local TMP_REPO_DIR="git"
+
+  local PRE_FUNCTION_PWD="$(pwd)"
+
+  local GIT_TEMP_FOLDER="$(mktemp -d)"
+
+  cd "${GIT_TEMP_FOLDER}"
+  mkdir ${TMP_REPO_DIR}
+  git clone "${GIT_URL}" "${TMP_REPO_DIR}"
+  cd ${TMP_REPO_DIR}
+  git checkout "${GIT_COMMIT}"
+
+  git archive HEAD | gzip > ${FINAL_TAR_GZ}
+
+  cd "${PRE_FUNCTION_PWD}"
+  rm -rf "${GIT_TEMP_FOLDER}"
+
+
+}
+
 RESCATUX_RELEASE_DIR="$(pwd)/rescatux-release"
 BASE_FILENAME="rescatux-`head -n 1 VERSION`"
-git archive HEAD | gzip > ${RESCATUX_RELEASE_DIR}/${BASE_FILENAME}.tar.gz
+git archive HEAD | gzip > ${RESCATUX_RELEASE_DIR}/source-code/${BASE_FILENAME}.tar.gz
+
+# chntpw
+
+CHNTPW_GIT_URL="https://github.com/rescatux/chntpw"
+CHNTPW_GIT_COMMIT="chntpw-ng-1.01"
+CHNTPW_GIT_NAME="chntpw"
+
+get_Git_Tar_Gz "${CHNTPW_GIT_URL}" "${CHNTPW_GIT_COMMIT}" "${RESCATUX_RELEASE_DIR}/source-code/${BASE_FILENAME}-${CHNTPW_GIT_NAME}-${CHNTPW_GIT_COMMIT}.tar.gz"
+
