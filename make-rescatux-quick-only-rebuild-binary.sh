a10a5d4bfaa6043bc0573a8ce7df1e1636da969d
adrian15 <adrian15sgd@gmail.com>
2018-11-17 18:26:43 +0100

    Rescatux make scripts now reference Rescatux and not Rescapp

--- make-rescatux-quick-only-rebuild-binary.sh
+++ make-rescatux-quick-only-rebuild-binary.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-# Rescapp make-rescatux-quick-only-rebuild-binary script
+# Rescatux make-rescatux-quick-only-rebuild-binary script
 # Copyright (C) 2012,2013,2014,2015,2016 Adrian Gibanel Lopez
 #
-# Rescapp is free software: you can redistribute it and/or modify
+# Rescatux is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Rescapp is distributed in the hope that it will be useful,
+# Rescatux is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Rescapp.  If not, see <http://www.gnu.org/licenses/>.
+# along with Rescatux.  If not, see <http://www.gnu.org/licenses/>.
 
 # This script assumes apt-get install live-helper has been done
 # This script assumes that the user has sudo permissions on lh build
