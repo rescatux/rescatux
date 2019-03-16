00dc525201ff87167bd6930b033fcb9c834fdfd2
adrian15 <adrian15sgd@gmail.com>
2019-03-16 21:44:30 +0100

    Start lxqt-config-monitor at Desktop boot and center it horizontally. This enables you to setup you to setup your screens even if you have two screens and one of them if broken.
    E.g. laptop screen broken with external screen plugged in.
    
    This script assumes the desktop will be stretched horizontally.

--- rescatux-extra/set-rescatux-background.sh
+++ rescatux-extra/set-rescatux-background.sh
@@ -22,3 +22,30 @@ sleep 2s # Wait for the systray / desktop to come up
 pcmanfm-qt --set-wallpaper "${MAGIC_BACKGROUND_PATH}"
 pcmanfm-qt --desktop &disown
 cmst --wait-time 5 --minimized &disown
+
+
+# Set monitor settings position - BEGIN
+MONITOR_SETTINGS_WINDOW_TITLE="Monitor Settings"
+
+lxqt-config-monitor > /dev/null 2>&1 &disown
+sleep 3s
+
+MONITOR1_WIDTH=$(xrandr --listactivemonitors | tail -n +2 | head -n 1 | awk '{print $3}' | awk -F '/' '{print $1}')
+MONITOR1_HALF_WIDTH="$(( ${MONITOR1_WIDTH} / 2 ))"
+MONITOR_COUNT=$(xrandr --listactivemonitors | tail -n +2 | wc -l)
+
+LXQT_CONFIG_MONITOR_WIDTH="$(wmctrl -l -G | grep "${MONITOR_SETTINGS_WINDOW_TITLE}" | awk '{print $5}')"
+LXQT_CONFIG_MONITOR_HALF_WIDTH="$(( ${LXQT_CONFIG_MONITOR_WIDTH} / 2 ))"
+
+if [ ${MONITOR_COUNT} -gt 1 ] ; then
+    # More than one monitor means we need to put the program between those two monitors
+    LXQT_CONFIG_MONITOR_NEW_X_OFFSET=$(( ${MONITOR1_WIDTH} - ${LXQT_CONFIG_MONITOR_HALF_WIDTH} ))
+else
+    # Only one monitor: Just center in the middle of the screen
+    LXQT_CONFIG_MONITOR_NEW_X_OFFSET=$(( ${MONITOR1_HALF_WIDTH} - ${LXQT_CONFIG_MONITOR_HALF_WIDTH} ))
+fi
+
+wmctrl -e 0,${LXQT_CONFIG_MONITOR_NEW_X_OFFSET},-1,-1,-1 -r "${MONITOR_SETTINGS_WINDOW_TITLE}"
+wmctrl -a "${MONITOR_SETTINGS_WINDOW_TITLE}"
+
+# Set monitor settings position - END
