7992011f96d16addd4563c638626490df0b1c5c7
adrian15 <adrian15sgd@gmail.com>
2026-03-28 18:46:24 +0100

    Added run-live-build.sh.

---
+++ scripts/run-live-build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+PLATFORM=$1
+ARCH=$(echo $PLATFORM | cut -d- -f1)
+
+ROOT=$(git rev-parse --show-toplevel)
+WORK="$ROOT/build/work/$PLATFORM"
+IMAGE="rescatux-builder-$ARCH"
+
+HOST_UID=$(id -u)
+HOST_GID=$(id -g)
+
+docker run --rm \
+  --privileged \
+  -e HOST_UID=$HOST_UID \
+  -e HOST_GID=$HOST_GID \
+  -v "$ROOT:/workspace" \
+  -w "/workspace/build/work/$PLATFORM" \
+  $IMAGE \
+  bash -c "
+     set -e
+     lb config
+     lb build
+     chown -R \$HOST_UID:\$HOST_GID /workspace/build/work/$PLATFORM
+  "
